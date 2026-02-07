72d439d55f234d30cd8953abc256fbb952b83a1c
Tharun-TU <tharuntu226@gmail.com>
2026-02-08 01:58:12 +0530

    Add goal icon support with persistence and unit tests

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -18,19 +18,16 @@ public class GoalControllerTests
     [Fact]
     public async void GetAll()
     {
-        // Arrange
         var goals = collections.GetGoals();
         var users = collections.GetUsers();
         IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
         IUsersService usersService = new FakeUsersService(users, users[0]);
         GoalController controller = new(goalsService, usersService);
 
-        // Act
         var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
         controller.ControllerContext.HttpContext = httpContext;
         var result = await controller.Get();
 
-        // Assert
         var index = 0;
         foreach (Goal goal in result)
         {
@@ -44,19 +41,16 @@ public class GoalControllerTests
     [Fact]
     public async void Get()
     {
-        // Arrange
         var goals = collections.GetGoals();
         var users = collections.GetUsers();
         IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
         IUsersService usersService = new FakeUsersService(users, users[0]);
         GoalController controller = new(goalsService, usersService);
 
-        // Act
         var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
         controller.ControllerContext.HttpContext = httpContext;
         var result = await controller.Get(goals[0].Id!);
 
-        // Assert
         Assert.IsAssignableFrom<Goal>(result.Value);
         Assert.Equal(goals[0], result.Value);
         Assert.NotEqual(goals[1], result.Value);
@@ -65,10 +59,24 @@ public class GoalControllerTests
     [Fact]
     public async void GetForUser()
     {
-        // Arrange
-        
-        // Act
-        
-        // Assert
+        var goals = collections.GetGoals();
+        var users = collections.GetUsers();
+
+        IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+        IUsersService usersService = new FakeUsersService(users, users[0]);
+        GoalController controller = new(goalsService, usersService);
+
+        var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+        controller.ControllerContext.HttpContext = httpContext;
+
+        var result = await controller.GetForUser(goals[0].UserId!);
+
+        Assert.NotNull(result);
+
+        foreach (Goal goal in result!)
+        {
+            Assert.IsAssignableFrom<Goal>(goal);
+            Assert.Equal(goals[0].UserId, goal.UserId);
+        }
     }
 }
